27333e7642c4868abff7635332298f44f69bace5
Denys <adriandutan15@gmail.com>
2020-05-11 15:18:05 -0500

    inicializacion de proyecto

---
+++ src/ec/edu/ups/idao/IUsuarioDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.idao;
+import java.util.List;
+import ec.edu.ups.modelo.Usuario; 
+/**
+ *
+ * @author Dutan2000
+ */
+public interface IUsuarioDAO {
+     //Métodos CRUD
+    public void create(Usuario usuario);
+    public Usuario read(String cedula);
+    public void update(Usuario usuario);
+    public void delete(Usuario usuario);   
+    public List<Usuario> findAll();
+}
