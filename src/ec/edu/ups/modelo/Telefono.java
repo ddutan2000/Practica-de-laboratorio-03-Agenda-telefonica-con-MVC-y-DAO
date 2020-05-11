2488343d441f3fd7914988151e10f09cbcf2fd98
Denys <adriandutan15@gmail.com>
2020-05-11 13:19:47 -0500

    inicializacion de proyecto

---
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author Dutan2000
+ */
+public class Telefono {
+    //atributos
+    private int codigo;
+    private String numero;
+    private String tipo;
+    private String operadora;
+    
+    //constructores
+
+    public Telefono() {
+    }
+    
+    //getters y setters
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+    
+}
