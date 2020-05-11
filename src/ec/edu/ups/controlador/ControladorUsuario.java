2f4878b5a9cbd13ede46761a55d95bac4f813671
Denys <adriandutan15@gmail.com>
2020-05-11 16:34:07 -0500

    inicializacion de proyecto

---
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.dao.UsuarioDAO;
+import ec.edu.ups.idao.IUsuarioDAO;
+import ec.edu.ups.modelo.Usuario;
+import ec.edu.ups.vista.VistaUsuario;
+import java.util.List;
+/**
+ *
+ * @author Dutan2000
+ */
+public class ControladorUsuario {
+    
+    //objeto vista 
+    private VistaUsuario vistaUsuario;
+    private Usuario usuario;
+    private IUsuarioDAO usuarioDAO;
+    
+    //constructor
+
+    public ControladorUsuario(VistaUsuario vistaUsuario) {
+        this.vistaUsuario = vistaUsuario;
+        this.usuarioDAO= new UsuarioDAO();
+        
+    }
+    
+    //metodo DAO para guardar un usuario
+    public void registrar(){
+       usuario= vistaUsuario.registrarusuario();
+       usuarioDAO.create(usuario);
+    }
+    
+    //llamar al DAO para obtener usuario por la cedula y luego muestra en la vista 
+    public void verUsuario(){
+        String cedula =vistaUsuario.buscarUsuario();
+        usuario=usuarioDAO.read(cedula);
+        vistaUsuario.verUsuario(usuario);
+    }
+    //llamar a DAO para actualizar un usuario
+    public void actualizar(){
+        usuario=vistaUsuario.modificarUsuario();
+        usuarioDAO.update(usuario);
+    }
+    
+    //eliminar un cliente 
+    public void eliminar(){
+        usuario = vistaUsuario.eliminarUsuario();
+        usuarioDAO.delete(usuario);
+        
+    }
+    
+    //llama al DAO para obtener todos los Usuarios y luego mostrar en vista
+    public void verUsuarios(){
+        List<Usuario>usuarios;
+        usuarios =usuarioDAO.findAll();
+        vistaUsuario.verUsuario(usuario);
+    }
+}
