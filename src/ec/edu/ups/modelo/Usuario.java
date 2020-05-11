f7501a9883de8621122bfae1a7a79ddb237998cc
Denys <adriandutan15@gmail.com>
2020-05-11 13:16:09 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/modelo/Usuario.java
+++ src/ec/edu/ups/modelo/Usuario.java
@@ -5,10 +5,105 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author Dutan2000
  */
 public class Usuario {
+    //atributos
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+    
+    //constructor
+
+    public Usuario() {
+    }
+
+    public Usuario(String cedula, String nombre, String apellido, String correo, String contraseña) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+    
+    //getters y setters
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    //metodos de clase object
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.cedula);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.cedula, other.cedula)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "\nUsuario" + "\nCedula: " + cedula + "\nNombre: " + nombre + "\nApellido: " + apellido + "\nCorreo=" + correo + "\nContraseña: " + contraseña ;
+    }
     
 }
