50768275864656b9737c749aeede03502d0fdebd
Denys <adriandutan15@gmail.com>
2020-05-13 08:14:40 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/controlador/ControladorUsuario.java
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -9,6 +9,10 @@ import ec.edu.ups.dao.UsuarioDAO;
 import ec.edu.ups.idao.IUsuarioDAO;
 import ec.edu.ups.modelo.Usuario;
 import ec.edu.ups.vista.VistaUsuario;
+import ec.edu.ups.dao.TelefonoDao;
+import ec.edu.ups.idao.ITelefono;
+import ec.edu.ups.modelo.Telefono;
+import ec.edu.ups.vista.VistaTelefono;
 import java.util.List;
 /**
  *
@@ -18,38 +22,49 @@ public class ControladorUsuario {
     
     //objeto vista 
     private VistaUsuario vistaUsuario;
+    private VistaTelefono vistaTelefono;
+    
+    //objeto model
     private Usuario usuario;
+    private Telefono telefono;
+    //objeto DAO
     private IUsuarioDAO usuarioDAO;
+    private ITelefono telefonoDAO;
     
     //constructor
 
-    public ControladorUsuario(VistaUsuario vistaUsuario) {
+    public ControladorUsuario(VistaUsuario vistaUsuario, IUsuarioDAO usuarioDAO,VistaTelefono vistaTelefono, ITelefono telefonoDAO) {
         this.vistaUsuario = vistaUsuario;
-        this.usuarioDAO= new UsuarioDAO();
+        this.usuarioDAO = usuarioDAO;
         
+        this.telefonoDAO = telefonoDAO;
+        this.vistaTelefono = vistaTelefono;
     }
+
+
+
     
     //metodo DAO para guardar un usuario
     public void registrar(){
-       usuario= vistaUsuario.registarTelefono();
+       usuario= vistaUsuario.registarUsuario();
        usuarioDAO.create(usuario);
     }
     
     //llamar al DAO para obtener usuario por la cedula y luego muestra en la vista 
     public void verUsuario(){
-        int codigo =vistaUsuario.buscarTelefono();
-        usuario=usuarioDAO.read(codigo);
-        vistaUsuario.verTelefono(usuario);
+        String cedula=vistaUsuario.buscarUsuario();
+        usuario=usuarioDAO.read(cedula);
+        vistaUsuario.verUsuario(usuario);
     }
     //llamar a DAO para actualizar un usuario
     public void actualizar(){
-        usuario=vistaUsuario.modificarTelefono();
+        usuario=vistaUsuario.modificarUsuario();
         usuarioDAO.update(usuario);
     }
     
     //eliminar un cliente 
     public void eliminar(){
-        usuario = vistaUsuario.eliminarTelefono();
+        usuario = vistaUsuario.eliminarUsuario();
         usuarioDAO.delete(usuario);
         
     }
@@ -58,6 +73,15 @@ public class ControladorUsuario {
     public void verUsuarios(){
         List<Usuario>usuarios;
         usuarios =usuarioDAO.findAll();
-        vistaUsuario.verTelefono(usuario);
+        vistaUsuario.verUsuario(usuarios);
+    }
+    
+    //metodo de agregacion de telefono
+    public void agregarTelefono(){
+        int codigo=vistaTelefono.buscarTelefono();
+        //probablemente un metodo de if si es que no exsiste el el telefono
+        telefono=telefonoDAO.read(codigo);
+        usuario.agregarTelefono(telefono);
+        usuarioDAO.update(usuario);
     }
 }
