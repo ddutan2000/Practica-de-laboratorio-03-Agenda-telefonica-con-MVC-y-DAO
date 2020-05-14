272c5c8226040b4b1ee69d32ae6c23df7d49b4d9
Denys <adriandutan15@gmail.com>
2020-05-14 14:06:54 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/test/Test.java
+++ src/ec/edu/ups/test/Test.java
@@ -41,7 +41,12 @@ public class Test {
 
         //diccionario
         Map<String, List<Usuario>> map = new HashMap<String, List<Usuario>>();
-
+        
+        controladorUsuario.registrar();
+        String correo = u.getContraseña();
+        String contrasenia = u.getContraseña();
+        String key = correo + contrasenia;
+        map.put(key, null);
         boolean salir = false;
         int opcion;
         //menu
@@ -57,52 +62,8 @@ public class Test {
 
                 switch (opcion) {
                     case 1:
-                        int opcion1;
-                        boolean salir1 = false;
-                        String key;
                         //registrar un usuario
                         controladorUsuario.registrar();
-                        String correo = u.getContraseña();
-                        String contrasenia = u.getContraseña();
-                        key = correo + contrasenia;
-                        map.put(key, null);
-                        //menu secudnario
-                        while (!salir1) {
-                            System.out.println("Que quiere hacer");
-                            System.out.println("1.Registrar Telefono\n2.Modificar Telefono"
-                                    + "\n3.Eliminar telefono\n4.Buscar Telefono\n5.Listar telefonos\n6.Salir");
-                            try {
-                                System.out.println("Elija una opcion");
-                                opcion1 = input.nextInt();
-
-                                switch (opcion1) {
-                                    case 1:
-                                        controladorTelefono.registrar();
-                                        break;
-                                    case 2:
-                                        controladorTelefono.actulizar();
-                                        break;
-                                    case 3:
-                                        controladorTelefono.eliminar();
-                                        break;
-                                    case 4:
-                                        controladorTelefono.verTelefono();
-                                        break;
-                                    case 5:
-                                        controladorTelefono.verTelefonos();
-                                        break;
-                                    case 6:
-                                        salir1 = true;
-                                        break;
-                                    default:
-                                        System.out.println("Solo numeros entre 1 y 6");
-
-                                }
-                            } catch (InputMismatchException e) {
-                                System.out.println("Debes ingresar un numero");
-                                input.nextInt();
-                            }
-                        }
                         break;
                     case 2:
 
@@ -111,11 +72,10 @@ public class Test {
                         System.out.println("ingrese contrseña");
                         String contrasenia1 = input.next();
                         String llave = correo1 + contrasenia1;
-                        if (map.containsKey(llave)) {
+                        if (map.containsKey(key)) {
                             int opcion2;
                             boolean salir2 = false;
                             //registrar un usuario
-                            controladorUsuario.registrar();
                             //menu secudnario
                             while (!salir2) {
                                 System.out.println("Que quiere hacer");
