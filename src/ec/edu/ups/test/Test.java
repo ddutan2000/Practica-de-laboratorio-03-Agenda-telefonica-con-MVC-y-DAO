6b209459229276896c7bb4cf276b53dcae1fa8d3
Denys <adriandutan15@gmail.com>
2020-05-14 12:56:20 -0500

    inicializacion de proyecto

--- src/ec/edu/ups/test/Test.java
+++ src/ec/edu/ups/test/Test.java
@@ -12,6 +12,9 @@ import ec.edu.ups.modelo.*;
 import ec.edu.ups.vista.*;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -21,6 +24,8 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //modelo
+        Usuario u = new Usuario();
 
         //vista
         VistaUsuario vistaU = new VistaUsuario();
@@ -33,23 +38,34 @@ public class Test {
         //controlador
         ControladorUsuario controladorUsuario = new ControladorUsuario(vistaU, usuarioDAO, vistaT, telefonoDAO);
         ControladorTelefono controladorTelefono = new ControladorTelefono(vistaT, telefonoDAO);
+
+        //diccionario
+        Map<String, List<Usuario>> map = new HashMap<String, List<Usuario>>();
+
         boolean salir = false;
         int opcion;
-
         //menu
         while (!salir) {
+            System.out.println("*********************");
             System.out.println("Bienvenido al programa de Denys");
             System.out.println("1.Registrarse \n2.Ya tengo cuenta\n3.Salir");
+            
             try {
                 System.out.println("Eliga una opcion");
+                System.out.println("*********************");
                 opcion = input.nextInt();
 
                 switch (opcion) {
                     case 1:
                         int opcion1;
                         boolean salir1 = false;
+                        String key;
                         //registrar un usuario
                         controladorUsuario.registrar();
+                        String correo = u.getContraseña();
+                        String contrasenia = u.getContraseña();
+                        key = correo + contrasenia;
+                        map.put(key, null);
                         //menu secudnario
                         while (!salir1) {
                             System.out.println("Que quiere hacer");
@@ -62,7 +78,6 @@ public class Test {
                                 switch (opcion1) {
                                     case 1:
                                         controladorTelefono.registrar();
-                                        
                                         break;
                                     case 2:
                                         controladorTelefono.actulizar();
@@ -87,61 +102,64 @@ public class Test {
                                 System.out.println("Debes ingresar un numero");
                                 input.nextInt();
                             }
-
                         }
+                        break;
                     case 2:
-                        
+
                         System.out.println("ingrese su correo electronico:");
-                        String correo = input.next();
-                        if (usuarioDAO.findAll().contains(correo)) {
-                            System.out.println("ingrese contrseña");
-                            String contrasenia = input.next();
-                            //falta hacer if () {
-                                int opcion2;
-                                boolean salir2 = false;
-                                //registrar un usuario
-                                controladorUsuario.registrar();
-                                //menu secudnario
-                                while (!salir2) {
-                                    System.out.println("Que quiere hacer");
-                                    System.out.println("1.Registrar Telefono\n2.Modificar Telefono"
-                                            + "\n3.Eliminar telefono\n4.Buscar Telefono\n5.Listar telefonos\n6.Salir");
-                                    try {
-                                        System.out.println("Elija una opcion");
-                                        opcion1 = input.nextInt();
+                        String correo1 = input.next();
+                        System.out.println("ingrese contrseña");
+                        String contrasenia1 = input.next();
+                        String llave = correo1 + contrasenia1;
+                        if (map.containsKey(llave)) {
+                            int opcion2;
+                            boolean salir2 = false;
+                            //registrar un usuario
+                            controladorUsuario.registrar();
+                            //menu secudnario
+                            while (!salir2) {
+                                System.out.println("Que quiere hacer");
+                                System.out.println("1.Registrar Telefono\n2.Modificar Telefono"
+                                        + "\n3.Eliminar telefono\n4.Buscar Telefono\n5.Listar telefonos\n6.Salir");
+                                try {
+                                    System.out.println("Elija una opcion");
+                                    opcion2 = input.nextInt();
 
-                                        switch (opcion1) {
-                                            case 1:
-                                                controladorTelefono.registrar();
-                                                break;
-                                            case 2:
-                                                controladorTelefono.actulizar();
-                                                break;
-                                            case 3:
-                                                controladorTelefono.eliminar();
-                                                break;
-                                            case 4:
-                                                controladorTelefono.verTelefono();
-                                                break;
-                                            case 5:
-                                                controladorTelefono.verTelefonos();
-                                                break;
-                                            case 6:
-                                                salir2 = true;
-                                                break;
-                                            default:
-                                                System.out.println("Solo numeros entre 1 y 6");
+                                    switch (opcion2) {
+                                        case 1:
+                                            controladorTelefono.registrar();
+                                            break;
+                                        case 2:
+                                            controladorTelefono.actulizar();
+                                            break;
+                                        case 3:
+                                            controladorTelefono.eliminar();
+                                            break;
+                                        case 4:
+                                            controladorTelefono.verTelefono();
+                                            break;
+                                        case 5:
+                                            controladorTelefono.verTelefonos();
+                                            break;
+                                        case 6:
+                                            salir2 = true;
+                                            break;
+                                        default:
+                                            System.out.println("Solo numeros entre 1 y 6");
 
-                                        }
-                                    } catch (InputMismatchException e) {
-                                        System.out.println("Debes ingresar un numero");
-                                        input.nextInt();
                                     }
+                                } catch (InputMismatchException e) {
+                                    System.out.println("Debes ingresar un numero");
+                                    input.nextInt();
                                 }
                             }
-                        } else {
-                            System.out.println("correo electronico no exsiste");
+                        }else{
+                            System.out.println("su correo o contraseña esta equivocado.");
                         }
+                        break;
+                    case 3:
+                        salir = true;
+                        break;
 
                 }
 
